ac090cd11d39db7fb785eb5ddcd2fec9a5d8051b
博高 <bogao.zx@alibaba-inc.com>
2015-12-04 19:27:05 +0800

    支持选取多个热区

--- js/main.js
+++ js/main.js
@@ -12,54 +12,118 @@ $(document).ready(function() {
 
 function capture(el) {
   var isDown = false;
-  var startX, startY, endX, endY;
+  var index = 0;
+  //被选区域在DOM中的位置及长宽
+  var top,left,width,height;
+  var results = [];
+  //保存每个矩形的位置
+  var rec = [];
 
   $(el).mousedown(function(e) {
+    top = $(el).offset().top;
+    left = $(el).offset().left;
+    width = $(el).width();
+    height = $(el).height();
     isDown = true;
-    startX = e.pageX;
-    startY = e.pageY;
-    makeDot(startX - $(el).offset().left, startY - $(el).offset().top);
+    rec.push({startX: e.pageX, startY: e.pageY});
+    makeDot(e.pageX - left, e.pageY - top, index);
   });
 
   $(el).mousemove(function(e) {
     if(isDown) {
-       makeRet((e.pageX - startX) + "px", (e.pageY - startY) + "px")
+       makeRet((e.pageX - rec[index].startX) + "px", (e.pageY - rec[index].startY) + "px", index)
     }
   });
 
   $(el).mouseup(function(e) {
 	  isDown = false;
-	  endX = e.pageX;
-	  endY = e.pageY;
-	  //alert(startY);
-	  var result = {
-  	    left: (((startX - $(el).offset().left) / $(el).width()) * 100).toFixed(2) + "%",
-  	    top: (((startY -$(el).offset().top) / $(el).height()) * 100).toFixed(2) + "%",
-  	    width: (((endX - startX) / $(el).width()) * 100).toFixed(2) + "%",
-  	    height: (((endY - startY) / $(el).height()) * 100).toFixed(2) + "%"
-      };
-    //alert(JSON.stringify(result));
-    setVaule(result);
+    setVaule(-1, updateResult(-1, rec[index].startX, rec[index].startY, e.pageX, e.pageY));
+    index++;
   });
 
-  function makeDot(x, y) {
-  	if($("#rectangle").length > 0) {
-      $("#rectangle").css({"left":x,"top":y,"height":"1px","width":"1px"});
-  	}
-  	else {
-      pointDiv = "<div style='height:1px;position:absolute;left:" + x +
-  	  ";top:" + y + "px;width:1px;background:rgba(0, 0, 0, 0.5);overflow:hidden;' id='rectangle'></div>"; 
-      $(el).parent().append(pointDiv);
-  	}
+  function makeDot(x, y, index) {
+    pointDiv = $("<div style='cursor:crosshair;height:1px;position:absolute;left:" + x + "px;top:" + y + 
+     "px;width:1px;background:rgba(0, 0, 0, 0.5);' id='rectangle-" + index +
+      "'><span class='close'>x</span></div>");
+    $(el).parent().append(pointDiv);
+
+    //设置点击x按钮时删除事件
+    pointDiv.children('.close').click(function() {
+      $parent = $(this).parent();
+      var pos = $parent.attr("id").match(/\d+$/);
+      setVaule(pos, null);
+      $parent.remove();
+    });
+
+    //区域拖曳实现
+    var iX, iY;
+    pointDiv.mousedown(function(e) {
+      iX = e.pageX;
+      iY = e.pageY;
+      $(this).css("border", "2px solid #eaeaea");
+      $(this).bind("mousemove", onMouseMove);
+      e.stopPropagation();
+    });
+    pointDiv.mouseup(function(e) {
+       $(this).unbind("mousemove",onMouseMove);
+       $(this).css("border", "");
+       var pos = $(this).attr("id").match(/\d+$/);
+       rec[pos].startX = e.pageX - iX + rec[pos].startX;
+       rec[pos].startY = e.pageY - iY + rec[pos].startY;
+       setVaule(pos, updateResult(pos, rec[pos].startX, rec[pos].startY));
+       e.stopPropagation();
+    });
+    function onMouseMove(e) {
+      var pos = $(this).attr("id").match(/\d+$/);
+      var newLeft = e.pageX - iX + rec[pos].startX - left;
+      var newTop = e.pageY - iY + rec[pos].startY - top;
+      $(this).css({"left":newLeft, "top":newTop});
+      e.stopPropagation();
+    }
   }
 
-  function makeRet(width, height) { 
-    $("#rectangle").css({"height":height,"width":width});
+  function makeRet(width, height, index) { 
+    $("#rectangle-" + index).css({"height":height,"width":width});
   }
-  
-}
 
-function setVaule(result) {
-  var content = result.width + " " + result.height + " " + result.left + " " + result.top;
-  $(".pbox textarea").val(content); 
+  function updateResult(index, startX, startY, endX, endY) {
+    if(endX && endY) {
+      result = {
+        'left': (((startX - left) / width) * 100).toFixed(2) + "%",
+        'top': (((startY - top) / height)* 100).toFixed(2) + "%",
+        'width': (((endX - startX) / width) * 100).toFixed(2) + "%",
+        'height': (((endY - startY) / height) * 100).toFixed(2) + "%"
+      };
+    }
+    else {
+      result['left'] = (((startX - left) / width) * 100).toFixed(2) + "%";
+      result['top'] = (((startY - top) / height)* 100).toFixed(2) + "%"
+      result['width'] = results[index].split(" ")[1];
+      result['height'] = results[index].split(" ")[2];
+    }
+
+    return result;
+  }
+
+  function setVaule(pos, result) {
+    if(pos > -1 && result == null) {
+      results.splice(pos, 1, "");
+    }
+    else {
+      var line = "# " + result.width + " " + result.height + " " + result.left + " " + result.top;
+      if(pos > -1) {
+        results.splice(pos, 1, line);
+      }
+      else {
+        results.push(line);
+      }
+    }
+    var content="";
+    for(var i in results) {
+      if(results[i] != "") {
+        content += (results[i] + "\n");
+      }
+    }
+    $(".pbox textarea").val(content); 
+  }
 }
